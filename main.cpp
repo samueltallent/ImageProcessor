f47bf09c5399487a8873cbd8b70cc997182d5698
samuel <samuel.tallent@utexas.edu>
2017-10-25 19:41:33 -0500

    Initial commit for risk reduction image processing

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main(int argc, char** argv) {
+	if ( argc != 2 )
+	{
+		printf("usage: DisplayImage.out <Image_Path>\n");
+		return -1;
+	}
+
+	Mat image;
+	image = imread( argv[1], 1 );
+
+	Mat grayScale;
+
+	cvtColor(image, grayScale, CV_RGB2GRAY);
+
+	if ( !image.data )
+	{
+		printf("No image data \n");
+		return -1;
+	}
+	namedWindow("Display Image", WINDOW_AUTOSIZE );
+	imshow("Display Image", grayScale);
+
+	waitKey(0);
+
+	return 0;
+}
