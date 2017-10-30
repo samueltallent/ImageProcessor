b411aa153d642e74b73d750e60a58fb89faf8720
samuel <samuel.tallent@utexas.edu>
2017-10-30 16:22:05 -0500

    Added more image transformations for risk reduction

--- main.cpp
+++ main.cpp
@@ -18,13 +18,31 @@ int main(int argc, char** argv) {
 
 	cvtColor(image, grayScale, CV_RGB2GRAY);
 
+	Mat equalized;
+
+	equalizeHist(grayScale, equalized); //see: https://docs.opencv.org/2.4/modules/imgproc/doc/histograms.html?highlight=equalizehist
+
+	Mat inverted;
+
+	bitwise_not(equalized, inverted); //see: http://answers.opencv.org/question/3639/subtract-from-white-invert/
+
+	Rect myROI(10,10,100,100); //crop: (x,y,width,height).  NOTE; program will crash if you provide width/height larger than the image
+
+	Mat croppedImage = inverted(myROI); //see: https://stackoverflow.com/questions/8267191/how-to-crop-a-cvmat-in-opencv
+
+
+
 	if ( !image.data )
 	{
 		printf("No image data \n");
 		return -1;
 	}
-	namedWindow("Display Image", WINDOW_AUTOSIZE );
-	imshow("Display Image", grayScale);
+//	namedWindow("Before", WINDOW_AUTOSIZE );
+	imshow("Original", image);
+	imshow("Grayscaled", grayScale);
+	imshow("Equalized", equalized);
+	imshow("Inverted", inverted);
+	imshow("Cropped", croppedImage);
 
 	waitKey(0);
 
